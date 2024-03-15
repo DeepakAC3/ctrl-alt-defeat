6184a68b9b536b9615c9c5dccef5ddb745c14722
DeepakAC3 <deepakpradip29@gmail.com>
2024-03-15 20:27:37 +0530

    inputs received

--- Hackathon/app.py
+++ Hackathon/app.py
@@ -6,7 +6,15 @@ import seaborn as sns
 import plotly.express as px
 
 
-input1 = streamlit.text_input("Enter input 1")
-input2 = streamlit.text_input("Enter input 2")
-input3 = streamlit.slider("Select input 3", min_value=0, max_value=10)
-input4 = streamlit.slider("Select input 4", min_value=0, max_value=10)
+age = streamlit.text_input("Enter your age:")
+smoke = streamlit.text_input("Enter 1 if you smoke, 0 if you don't:")
+chronic = streamlit.text_input("Enter 1 if you have a chronic disease, 2 if you don't:")
+wheezing = streamlit.text_input("Enter 1 if you have wheezing, 2 if you don't:")
+alcohol = streamlit.text_input("Enter 1 if you drink alcohol, 2 if you don't:")
+coughing = streamlit.text_input("Enter 1 if you have coughing, 2 if you don't:")
+shortness_of_breath = streamlit.text_input("Enter 1 if you have shortness of breath, 2 if you don't:")
+swallowing_difficulty = streamlit.text_input("Enter 1 if you have swallowing difficulty, 2 if you don't:")
+chest_pain = streamlit.text_input("Enter 1 if you have chest pain, 2 if you don't:")
+
+
+# Path: Hackathon/app.py
