fc48d180d65befed0f7c9e420432fd2ce2b12177
DeepakAC3 <deepakpradip29@gmail.com>
2024-03-15 20:35:54 +0530

    .

--- Hackathon/app.py
+++ Hackathon/app.py
@@ -1,9 +1,9 @@
 import streamlit
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.express as px
+# import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# import plotly.express as px
 
 
 age = streamlit.text_input("Enter your age:")
