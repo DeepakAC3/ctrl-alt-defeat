121fa0889cc175582b834917cc2e44d0b4a879b5
DeepakAC3 <deepakpradip29@gmail.com>
2024-03-15 22:29:55 +0530

    first website

--- Hackathon/helo.py
+++ Hackathon/helo.py
@@ -15,3 +15,9 @@ st.text("Hello Text")
 #success,info
 st.success("Executed Successfully")
 st.info("Information")
+st.warning("This is a warning")
+st.error("Error was encountered")
+
+#write
+st.write("I'm writing this sample code")
+st.write(range(20))
