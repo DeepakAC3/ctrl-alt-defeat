46991e5b177eef2a84d4b39f27a436b3d8d58416
DeepakAC3 <deepakpradip29@gmail.com>
2024-03-15 19:59:33 +0530

    app creation

---
+++ Hackathon/app.py
@@ -0,0 +1,10 @@
+import streamlit
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+streamlit.title('Hackathon')
+streamlit.write('This is a hackathon project')
+
